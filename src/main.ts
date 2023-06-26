faeecda7523be61c3e9160ae72320d936a9500cd
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-06-26 18:31:10 +0200

    Added functionality to todo list form

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,27 @@
-const appContainer = document.getElementById('app') as HTMLDivElement | null;
+const todoList = document.getElementById('todo-list') as HTMLUListElement;
+const todoForm = document.getElementById('todo-form') as HTMLFormElement;
+const todoInput = document.getElementById('todo-input') as HTMLInputElement;
 
-appContainer?.appendChild(document.createTextNode("Hello World!"));
+const createTodo = (todoText: string): HTMLLIElement => {
+  const li: HTMLLIElement = document.createElement('li');
+  const deleteBtn: HTMLButtonElement = document.createElement('button');
+
+  li.innerText = todoText;
+  deleteBtn.innerText = 'X';
+  deleteBtn.addEventListener('click', () => todoList.removeChild(li));
+  li.appendChild(deleteBtn);
+
+  return li;
+}
+
+todoForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const todoText: string = todoInput.value;
+
+  const todoListItem: HTMLLIElement = createTodo(todoText);
+
+  todoList.appendChild(todoListItem);
+
+  todoInput.value = "";
+});
