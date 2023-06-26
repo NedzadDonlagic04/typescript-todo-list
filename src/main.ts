dcd5842d63e4b4dd816e56ec21d032d25957a185
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-06-26 21:32:26 +0200

    Changed code to be safer and more future proof

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-const todoList = document.getElementById('todo-list') as HTMLUListElement;
-const todoForm = document.getElementById('todo-form') as HTMLFormElement;
-const todoInput = document.getElementById('todo-input') as HTMLInputElement;
+function init() {
+  const todoList = document.getElementById('todo-list') as HTMLUListElement | null;
+  const todoForm = document.getElementById('todo-form') as HTMLFormElement | null;
+  const todoInput = document.getElementById('todo-input') as HTMLInputElement | null;
 
-const createTodo = (todoText: string): HTMLLIElement => {
+  if (!todoList) throw new Error("Could not find element with id todo-list");
+  if (!todoForm) throw new Error("Could not find element with id todo-form");
+  if (!todoInput) throw new Error("Could not find element with id todo-input");
+
+  return { todoList, todoForm, todoInput };
+}
+
+const { todoList, todoForm, todoInput } = init();
+
+function createTodo(todoText: string): HTMLLIElement {
   const li: HTMLLIElement = document.createElement('li');
   const deleteBtn: HTMLButtonElement = document.createElement('button');
 
